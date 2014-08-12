728009f090b5b762b633afae7da27f74cd038d2f
Jacob Smith <jacob@jacobsmith.io>
2014-08-12 11:30:41 -0400

    Get it working with ghost-portal

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express'),
+    session = require('express-session'),
+    MongoStore = require('connect-mongo')(session),
+    ghostPortal = require('ghost-portal');
+
+require('dotenv').load();
+var ENV = process.env;
+
+var app = express();
+
+console.log("Connecting to " + ENV.MONGO_URL);
+app.use(session({
+  secret: ENV.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  store: new MongoStore({
+    url: ENV.MONGO_URL
+  })
+}));
+
+app.use(ghostPortal({
+  google: { requiredDomains: ['theironyard.com'] }
+}));
+
+var port = Number(ENV.PORT || 8888);
+app.listen(port);
+console.log("Ghost Ring is listening on port " + port);
