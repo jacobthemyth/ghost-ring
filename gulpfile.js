9b642f1c8014c2e2f0f805564e1912237c8e8c74
Mason Stewart <masondesu@gmail.com>
2014-08-19 15:27:41 -0400

    Don't obliterate the sites' sidebars anymore

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,16 @@
-var gulp = require('gulp');
-var ghostHelm = require('ghost-helm');
+var gulp = require('gulp'),
+    ghostHelm = require('ghost-helm'),
+    inject = require('gulp-inject');
+
+gulp.task('inject-sidebar', ['sidebar'], function(){
+  return gulp.src(config.distDir + '/**/*.html')
+    .pipe(inject(gulp.src(config.sidebar), {
+      name: 'ghost-ring-sidebar',
+      transform: function (filePath, file) {
+        return file.contents.toString('utf8')
+      }
+    }))
+    .pipe(gulp.dest(config.distDir));
+});
+
 ghostHelm.setup({cleanDir: ['.tmp']}, gulp);
